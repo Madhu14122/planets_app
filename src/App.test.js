915f4d8ca5e26838b6ca39602cfe08ed3c56b767
Madhu14122 <coolcoder14122@gmail.com>
2023-08-19 22:35:59 +0530

    first-comit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import '@testing-library/jest-dom'
+import {configure} from '@testing-library/react'
+
+global.matchMedia =
+  global.matchMedia ||
+  function () {
+    return {
+      matches: false,
+      addListener: function () {},
+      removeListener: function () {},
+    }
+  }
+
+configure({testIdAttribute: 'data-testid'})
